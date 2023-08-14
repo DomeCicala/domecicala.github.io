7fb1ec25c823cfc9608f098e05b583b89b7d06be
DomeCicala <142233856+DomeCicala@users.noreply.github.com>
2023-08-14 18:49:02 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 const navDropShad = document.getElementById("drop");
 
+
+let maxNavHeight = window.innerHeight * 26.88 / 100;
+let minNavHeight = window.innerHeight * 7.53 / 100;
+
 addEventListener("scroll",calcNavHeight);
+addEventListener("resize",resizeCalc);
+
+async function resizeCalc() {
+    maxNavHeight = window.innerHeight * 26.88 / 100;
+    minNavHeight = window.innerHeight * 7.53 / 100;
+}
 
 async function calcNavHeight() {
-    let maxNavHeight = window.innerHeight * 26.88 / 100;
-    let minNavHeight = window.innerHeight * 7.53 / 100;
     let y = document.body.getBoundingClientRect().y;
     if (y < -500) return;
     let calc = maxNavHeight + y * .36;
