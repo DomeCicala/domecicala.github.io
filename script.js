ad8f586a56df099353c753a91f6851d4743c4209
DomeCicala <142233856+DomeCicala@users.noreply.github.com>
2023-08-14 17:56:13 +0200

    test

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ const navDropShad = document.getElementById("drop");
 addEventListener("scroll", calcNavHeight);
 
 async function calcNavHeight()  {
-    let y = ~document.body.getBoundingClientRect().y;
-    if (y < -250) return;
-    let calc = 250 - y / 2.8;
+    let y = document.body.getBoundingClientRect().y;
+    if (y < -500) return;
+    let calc = 250 + y * .36;
+    console.log(y, "|", calc);
     if (calc < 70) calc = 70;
     navDropShad.style.height = `${calc}px` 
 }
